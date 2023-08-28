a57d02189aad152fbcd29af9c5a17d1c3e34fa02
JT4848 <jtpricone@gmail.com>
2023-08-27 20:16:33 -0400

    register and login working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route, Link } from "react-router-dom"
 import Home from "./Components/Home.jsx"
 import User from "./Components/User.jsx"
@@ -10,18 +9,20 @@ const App = () => {
   return (
     <>
       <div id="container">
-        <h1></h1>
+          <div id= "MainHeading-container">
+            <h1 id="MainHeading">List of Useless Items</h1>
+          </div>
         <div id="navbar">
-          <Link to="/">
+          <Link to="/" className="linkstyle">
             Home
           </Link>
-          <Link to="/login">
+          <Link to="/login" className="linkstyle">
             User Login
           </Link>
-          <Link to="/cart">
+          <Link to="/cart" className="linkstyle">
             Shopping Cart
           </Link>
-          <Link to="/register">
+          <Link to="/register" className="linkstyle">
             New User Registration
           </Link>
         </div>
--- server/index.js
+++ server/index.js
@@ -7,7 +7,19 @@ const PORT = 3009;
 
 app.use(require("body-parser").json());
 
+app.use((req, res, next) => {
+  const auth = req.headers.authorization;
 
+  const token = auth?.startsWith("Bearer ") ? auth.slice(7): null;
+
+  try{
+    const {id} = jwt.verify(token, process.env.JWT);
+    req.userid = id;
+  }catch{
+    req.userId = null;
+  }
+  next();
+})
 
 app.use('/auth', require('./auth'))
 
