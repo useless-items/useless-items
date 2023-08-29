9298e37c1f6d3e0f51b7bdbc4dfd0d1bd5c570c9
Nathan Kim <novicenate@pop-os.localdomain>
2023-08-29 12:44:53 -0400

    made adjustments to Register.jsx to start permitting authenticaton functionality

--- client/src/Components/Cart.jsx
+++ client/src/Components/Cart.jsx
@@ -1,9 +1,36 @@
-const Cart = () => {
-  return (
-    <div className="Cart">
-      <h1>This will be a shopping cart</h1>
-    </div>
-  );
-}
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
+const Cart = () => {
+
+    return (
+        <>
+            <div>
+                <Link to='/'>Home</Link>
+            </div>
+
+            <section className='cart'>
+                <h1>Cart</h1>
+
+                <div className='container'>
+                    {cartItems.map((product) => (
+                        <div className='product' key={product.id}>
+                            <img src={product.productImgUrl} alt={product.productName} />
+                            <h3>{product.productName}</h3>
+                            <h4>Price: ${product.pennies /100}</h4>
+                        </div>
+                    ))}
+                </div>
+
+                <div className='total'>
+                        <h4>Total Price: ${totalPrice}</h4>
+                        <button>Check Out</button>
+                </div>
+            </section>
+            
+        </>
+    )
+
+}
+  
 export default Cart;
