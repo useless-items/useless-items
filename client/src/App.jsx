d440446810e1ee68e8ef5dfb0c6432ca0811f591
Eddie Jacobian <edjacobian@gmail.com>
2023-08-29 14:18:55 -0400

    made cart visible dummy data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route, Link } from "react-router-dom"
-import Home from "./Components/Home.jsx"
-import Login from "./Components/Login.jsx"
-import Cart from "./Components/Cart.jsx"
-import Register from "./Components/Register.jsx"
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Components/Home.jsx';
+import Login from './Components/Login.jsx';
+import Cart from './Components/Cart.jsx';
+import Register from './Components/Register.jsx';
 
 const App = () => {
- 
   return (
     <>
       <div id="container">
-          <div id= "MainHeading-container">
-            <h1 id="MainHeading">List of Useless Items</h1>
-          </div>
+        <div id="MainHeading-container">
+          <h1 id="MainHeading">List of Useless Items</h1>
+        </div>
         <div id="navbar">
           <Link to="/" className="linkstyle">
             Home
@@ -30,14 +30,41 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/cart" element={<Cart />} />
+            {/* Pass cartItems and totalPrice to Cart component */}
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  cartItems={dummyCartItems}
+                  totalPrice={dummyTotalPrice}
+                />
+              }
+            />
             <Route path="/register" element={<Register />} />
           </Routes>
         </div>
       </div>
-     </>
+    </>
+  );
+};
 
-  )
-}
+// Define dummy cart items and total price here
+const dummyCartItems = [
+  {
+    id: 1,
+    productName: 'Item 1',
+    productImgUrl: 'image-url-1',
+    price: 10.99,
+  },
+  {
+    id: 2,
+    productName: 'Item 2',
+    productImgUrl: 'image-url-2',
+    price: 15.99,
+  },
+  // ... add more items
+];
 
-export default App
+const dummyTotalPrice = 26.98; // Example total price
+
+export default App;
