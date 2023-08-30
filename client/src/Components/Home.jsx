bc9171ade5731111776467cc4c21cfaba26dfe25
Nathan Kim <novicenate@pop-os.localdomain>
2023-08-30 12:45:56 -0400

    initial commit; attempting to add products onto the homepage

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 const Home = () => {
   // State to store the search query
   const [searchQuery, setSearchQuery] = useState("");
+  const [product, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    const fetchProducts = async() => {
+      await prisma.product.findMany().then((response) => {
+        setProducts(response);
+      });
+    }
+      
+    fetchProducts();
+  }, []);
 
   // Event handler to update the search query
   const handleSearchChange = (event) => {
@@ -11,14 +25,25 @@ const Home = () => {
 
   return (
     <div>
-      <h2 id="FindItems">Find Some Weird Items:</h2>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
-      {/* Display search results or content */}
+        <h2 id="FindItems">Find Some Weird Items:</h2>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        {/* Display search results or content */}
+        <div>
+          {products.map((product) => (
+            <div key={product.id}>
+              <img src={product.productImgUrl} alt={product.productName} />
+              <h3>{product.productName}</h3>
+              <p>{product.description}</p>
+              <h4>Rating: {product.productRating}</h4>
+              <h5>Price: {product.pennies /100}</h5>
+            </div>
+          ))}
+        </div>
     </div>
   );
 };
