4336a2bb370356d9ee8ac7ee99a52982500c4af0
Eddie Jacobian <edjacobian@gmail.com>
2023-08-30 01:41:10 -0400

    brought JWT to shopping cart/functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Components/Home.jsx';
 import Login from './Components/Login.jsx';
@@ -7,6 +7,8 @@ import Register from './Components/Register.jsx';
 import Checkout from './Components/Checkout.jsx';
 
 const App = () => {
+  const [token, setToken] = useState(null);
+
   return (
     <>
       <div id="container">
@@ -29,48 +31,31 @@ const App = () => {
         </div>
         <div id="main-section">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            {/* Pass cartItems and totalPrice to Cart component */}
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login setToken={setToken} />}
+            />
             <Route
               path="/cart"
-              element={
-                <Cart
-                  cartItems={dummyCartItems}
-                  totalPrice={dummyTotalPrice}
-                />
-              }
+              element={<Cart token={token} />}
             />
-            {/* Route for the Checkout component */}
             <Route
-            path="/checkout"
-            element={<Checkout totalPrice={dummyTotalPrice} />}
+              path="/register"
+              element={<Register setToken={setToken} />}
             />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/checkout"
+              element={<Checkout />}
+            />
           </Routes>
         </div>
       </div>
     </>
   );
 };
-
-// Define dummy cart items and total price here
-const dummyCartItems = [
-  {
-    id: 1,
-    productName: 'Item 1',
-    productImgUrl: 'image-url-1',
-    price: 10.99,
-  },
-  {
-    id: 2,
-    productName: 'Item 2',
-    productImgUrl: 'image-url-2',
-    price: 15.99,
-  },
-  // ... add more items
-];
-
-const dummyTotalPrice = 26.98; // Example total price
 
 export default App;
