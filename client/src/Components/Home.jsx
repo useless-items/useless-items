3665fec630e39f7aa0d1e44ed9622790284d3763
Eddie Jacobian <edjacobian@gmail.com>
2023-09-05 13:01:46 -0400

    corrections to fix minor last min bugs

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -51,7 +51,7 @@ const Home = ({ addToCart, cartCounter, setCartCounter }) => {
             <h1>{product.productName}</h1>
             <h3> ${(product.pennies / 100).toFixed(2)}</h3>
             <h3>Description: {product.description}</h3>
-            <img src={products.productImgUrl} />
+            <img src={product.productImgUrl} />
             <h3>Rating: {product.productRating}</h3>
             <h3>Stock: {product.stock}</h3>
             <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
