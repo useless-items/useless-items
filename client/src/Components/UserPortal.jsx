416127b80bd38d6772f035b9fc9a5a8676e1288f
Eddie Jacobian <edjacobian@gmail.com>
2023-09-05 13:40:21 -0400

    final final styling

--- client/src/Components/UserPortal.jsx
+++ client/src/Components/UserPortal.jsx
@@ -88,7 +88,7 @@ const handleProductForm = () => {
 return (
   <>
     <h1 id="portalfont">User Portal</h1>
-    <button onClick={handleProductForm}>Post Product</button>
+    <button onClick={handleProductForm} id="portalbutton">Post Product</button>
     {isProductFormOpen && <ProductForm handleProductSubmit={handleProductSubmit} />}
     {allProducts.map((product) => {
       const isUserProduct = product.user.id === userId; 
@@ -98,7 +98,7 @@ return (
           <h1>{product.productName}</h1>
           <h3>Price: {product.pennies}</h3>
           <h3>Description: {product.description}</h3>
-          <h3>{product.productImgUrl}</h3>
+          <img src={product.productImgUrl} />
           <h3>Rating: {product.productRating}</h3>
           <h3>Stock: {product.stock}</h3>
           {isUserProduct && (
